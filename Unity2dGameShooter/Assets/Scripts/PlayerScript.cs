04016047fb0dd2461319d84dc017f978334ea68f
Dave Arneson <urvaius@gmail.com>
2013-11-22 23:07:09 -0800

    working on tutorial

---
+++ Unity2dGameShooter/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+/// <summary>
+/// Player control and Movement
+/// </summary>
+public class PlayerScript : MonoBehaviour {
+    /// <summary>
+    /// 1 - speed
+    /// </summary>
+
+    public Vector2 speed = new Vector2(50, 50);
+
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // 2 - retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // 3 -movement per direction
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+        // 4 -Relative to the time
+        movement *= Time.deltaTime;
+
+        // 5 -move the game object
+        transform.Translate(movement);
+
+	
+	}
+}
