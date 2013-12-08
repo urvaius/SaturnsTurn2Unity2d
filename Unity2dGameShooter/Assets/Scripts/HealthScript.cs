cf59b48334932ba81cf627129ec32716a3663789
Dave Arneson <urvaius@gmail.com>
2013-12-07 22:51:15 -0800

    continuing

--- Unity2dGameShooter/Assets/Scripts/HealthScript.cs
+++ Unity2dGameShooter/Assets/Scripts/HealthScript.cs
@@ -34,6 +34,7 @@ public class HealthScript : MonoBehaviour
                 if (hp <= 0)
                 {
                     // Dead!
+                    SpecialEffectsHelper.Instance.Explosion(transform.position);
                     Destroy(gameObject);
                 }
             }
